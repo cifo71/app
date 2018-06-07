bd1579e23211db5c3eb052a7f270f07479085b7e
Andy <andrejmikulicka95@gmail.com>
2018-06-07 17:21:53 +0200

    Kod upratany, obrazok nalinkovany

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,62 @@
 <template>
   <div id="app">
-    <div class="header">
+    <header class="header">
       <router-link :to="{ name: 'home' }">
-      <img :src="'/static/img/logo.png'" />
+        <img src="@/assets/img/logo.png" />
       </router-link>
-      <div class="header-right">
-        <a  href="#home">Home</a>
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
-      </div>
-      </div>
-      
+      <nav>
+        <a href="#home">home</a>
+        <a href="#contact">contact</a>
+        <a href="#about">about</a>
+      </nav>
+    </header>
 
-    <router-view/>
+    <section>
+      <router-view/>
+    </section>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import "~@/assets/scss/reset";
+@import "~@/assets/scss/variables";
+@import "~@/assets/scss/mixins";
 
-/* Style the header with a grey background and some padding */
 .header {
-  overflow: hidden;
-  background-color: #f1f1f1;
-  height: 20px;
-  padding: 20px 10px;
-}
+  width:           100%;
+  height:          80px;
+  padding:         0 40px;
+  display:         flex;
+  align-items:     center;
+  justify-content: space-between;
+  position:        fixed;
+  top:             0;
+  left:            0;
 
-/* Style the header links */
-.header a {
-  float: left;
-  color: black;
-  text-align: center;
-  padding: 12px;
-  text-decoration: none;
-  font-size: 18px; 
-  line-height: 25px;
-  border-radius: 4px;
-}
-
-/* Change the background color on mouse-over */
-.header a:hover {
-  background-color: #ddd;
-  color: black;
-}
-
-/* Style the active/current link*/
-.header a.active {
-  background-color: dodgerblue;
-  color: white;
-}
-
-/* Float the link section to the right */
-.header-right {
-  float: right;
-}
-
-/* Add media queries for responsiveness - when the screen is 500px wide or less, stack the links on top of each other */ 
-@media screen and (max-width: 500px) {
-  .header a {
-    float: none;
-    display: block;
-    text-align: left;
+  img {
+    max-width: 100px;
   }
-  .header-right {
-    float: none;
+
+  nav {
+    display: none;
+    @include media(md) {
+      display: flex;
+    }
+
+    a {
+      padding:   12px;
+      color:     $accent;
+      font-size: 18px;
+      display:   block;
+
+      &:not(:last-of-type) {
+        margin-right: 60px;
+      }
+      
+      &:hover {
+        color: $white;
+      }
+    }
   }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>
-    <input type="text" placeholder="Poznamka..." v-model="todo" />
-    <button type="button" @click="addTodo()">Pridaj</button>
-    <div v-for="(todo, index) in todoList" :key="index">
-    {{ todo }}
+  <div class="home">
+    <div class="home__inner">
+      <input type="text" placeholder="Poznamka..." v-model="todo" />
+      <button type="button" @click="addTodo()">Pridaj</button>
+      <div v-for="(todo, index) in todoList" :key="index">
+        {{ todo }}
+      </div>
     </div>
   </div>
 </template>
@@ -12,13 +14,13 @@
 export default {
   data() {
     return {
-      todo: "",
+      todo: '',
       todoList: []
     };
   },
   methods: {
     addTodo() {
-      this.todoList.push(this.todo);
+      this.todoList.push(this.todo)
       this.todo = ''
     }
   }
@@ -26,4 +28,45 @@ export default {
 </script>
 
 <style lang="scss">
+@import "~@/assets/scss/variables";
+@import "~@/assets/scss/mixins";
+
+.home {
+  min-height:      100vh;
+  background:      $primary;
+  display:         flex;
+  align-items:     center;
+  justify-content: center;
+
+  &__inner {
+    display:        flex;
+    flex-direction: column;
+
+    input {
+      width:         260px;
+      margin-bottom: 20px;
+      padding:       10px 20px;
+      background:    lighten($primary, 7%);
+      border:        none;
+      border-radius: 6px;
+      color:         $accent;
+      font-size:     28px;
+      @include media(sm) {
+        width: 400px;
+      }
+      @include media(md) {
+        width: 600px;
+      }
+    }
+
+    button {
+      padding:        10px 20px;
+      background:     $secondary;
+      border-radius:  6px;
+      color:          $primary;
+      font-size:      20px;
+      font-weight:    700;
+    }
+  }
+}
 </style>
